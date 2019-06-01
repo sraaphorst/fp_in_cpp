7cc4aee3de3554b3aa9f0128e6b829377ae2626c
Sebastian Raaphorst <srcoding@gmail.com>
2019-06-01 12:55:06 -0400

    partition vs stable_partition.

---
+++ src/ch02/listing2_5.cpp
@@ -0,0 +1,57 @@
+/**
+ * listing2_5.cpp
+ *
+ * By Sebastian Raaphorst, 2019.
+ */
+
+#include <iostream>
+#include <vector>
+
+#include <algorithm>
+#include <iterator>
+
+enum Gender { MALE, FEMALE };
+
+// We need to be able to swap elements, so these cannot be const.
+struct Person {
+    std::string name;
+    Gender sex;
+};
+
+bool is_woman(const Person &p) {
+    return p.sex == FEMALE;
+}
+
+int main() {
+    // Since we are changing the vector, this cannot be const.
+    std::vector<Person> people = {
+            {"Joshua",      MALE},
+            {"Janice",      FEMALE},
+            {"George",      MALE},
+            {"Alexander",   MALE},
+            {"Judith",      FEMALE},
+            {"Layla",       FEMALE},
+            {"Christopher", MALE}
+    };
+
+    // Make a copy of the vector to show the difference between partition and stable_partirtion:
+    std::vector<Person> people2 = people;
+
+    // PARTITION: Order is not naturally kept.
+    auto iter1 = std::partition(std::begin(people), std::end(people), is_woman);
+    std::cout << "Women (unstable):\n";
+    for (auto i = std::begin(people); i != iter1; ++i)
+        std::cout << '\t' << i->name << '\n';
+    std::cout << "Men (unstable):\n";
+    for (auto i = iter1; i != std::end(people); ++i)
+        std::cout << '\t' << i->name << '\n';
+
+    // STABLE PARTITION: Order is kept.
+    auto iter2 = std::stable_partition(std::begin(people2), std::end(people2), is_woman);
+    std::cout << "Women (stable):\n";
+    for (auto i = std::begin(people2); i != iter2; ++i)
+        std::cout << '\t' << i->name << '\n';
+    std::cout << "Men (stable):\n";
+    for (auto i = iter2; i != std::end(people2); ++i)
+        std::cout << '\t' << i->name << '\n';
+}
